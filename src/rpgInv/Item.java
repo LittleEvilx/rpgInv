45d8e1980e379b692f4c5871732a7de44848d338
LittleEvil <91768597+LittleEvilx@users.noreply.github.com>
2024-03-05 22:11:29 -0700

    go

--- src/rpgInv/Item.java
+++ src/rpgInv/Item.java
@@ -10,21 +10,47 @@ public class Item
 	int defence;
 	int missPer;
 	int tag;
+	/*
+	 Weapon = 0
+	 Shield = 1
+	 Armor = 2
+	*/
+	int refChance;
+	int refDamage;
+	int index;
 	boolean held;
 	boolean picked;
 	
-	public Item(String name, int damage, int cc, int cd, int defence, int missPer, int tag) 
+	public Item(String name, int damage, int cc, int cd, int missPer)
 	{
 		this.name = name;
 		this.damage = damage;
 		this.cc = cc;
-		this.cd = cd; 
-		this.defence = defence;
-		this.missPer = missPer;
-		this.tag = tag;
-		this.picked = false;
-		this.held = false;
+		this.cd = cd;
+		tag = 0;
+		picked = true;
+		held = false;
+		index = 0;
 	}
-
+	
+	public Item(String name, int missPer, int refDamage, int refChance)
+	{
+		this.name = name;
+		this.missPer = missPer;
+		this.refChance = refChance;
+		this.refDamage = refDamage;
+		tag = 1;
+		picked = true;
+		held = false;
+	}
+	
+	public Item(String name, int defence)
+	{
+		this.name = name;
+		this.defence = defence;
+		tag = 2;
+		picked = true;
+		held = false;
+	}
 	
 }
--- src/rpgInv/Main.java
+++ src/rpgInv/Main.java
@@ -8,8 +8,14 @@ public class Main
 
 	static Scanner scanner = new Scanner(System.in);
 	
-	static Item sword1 = new Item("sword1",5,2,10,0,5,6);
-	static Item sword2 = new Item("sword2",10,2,0,0,26,2); 
+	static Item sword1 = new Item("sword1",2,50,150,25);
+	static Item sword2 = new Item("sword2",5,20,200,90);
+	static Item shield1 = new Item("shield1",20,10,50);
+	static Item shield2 = new Item("shield2",50,0,0);
+	static Item armor1 = new Item("armor1",5);
+	static Item armor2 = new Item("armor2",10);
+	
+	static int i = 0;
 	
 	static ArrayList<Item> items = new ArrayList<Item>();
 	
@@ -18,13 +24,19 @@ public class Main
 		
 		items.add(sword1);
 		items.add(sword2);
+		items.add(shield1);
+		items.add(shield2);
+		items.add(armor1);
+		items.add(armor2);
 		
-		checkInv();
 		
-		pickup(sword1);
+		while(true)
+		{
+			int x = scanner.nextInt();
 		
-		checkInv();
-		
+			if(x == 0) checkInv();
+			else if(x == 1) pickup(items.get(rand(0,5)));
+		}
 	}
 	
 	public static void pickup(Item x)
@@ -37,10 +49,59 @@ public class Main
 	
 	public static void checkInv()
 	{
-		items.forEach((i) -> 
+		i = 0;
+		
+		int x;
+		
+		items.forEach((e) -> 
 		{
-			if(i.picked) System.out.println(i.name);
+			
+			if(e.picked) 
+			{
+				i++;
+				if(e.tag == 0 ) System.out.println("[Weapon] " + i + ". " + e.name);
+				else if(e.tag == 1 ) System.out.println("[Shield] " + i + ". " + e.name);
+				else System.out.println("[Armor] " + i + ". " + e.name);
+				
+				e.index = i;
+			}
 		});
+		
+		x = scanner.nextInt();
+		if (x <= i && x >= 0)
+		{
+			hold(findIndex(x));
+		}
 	}
+	
+	public static void hold(Item item)
+	{
+			System.out.println(item);
+		
+			items.forEach((e) -> 
+			{
+				if(e.held && e.tag == item.tag) e.held = false;
+			});
+			item.held = true;
+			System.out.println("You equiped " + item.name);
+		
+	}
+	
+	public static Item findIndex(int x)
+	{
+		for (int i = items.size()-1; i>=0; i--)
+		{
+			if(items.get(i).index == x) return items.get(i);
+		}
+		System.out.println("Fatal Error: Failed to Index!");
+		System.exit(0);
+		return null;
+	}
+	
+	public static int rand(int min, int max)
+	{
+		return (int)Math.floor(Math.random() * (max - min + 1) + min);
+	}
+	
 
 }
